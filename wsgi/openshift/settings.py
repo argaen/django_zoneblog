493ed656c805dfa9ef357cd5cd68655db672058b
argaen <manuel.miranda@urv.cat>
2014-06-12 17:41:35 +0200

    Fixing email bug.

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -128,21 +128,3 @@ INSTALLED_APPS = (
     'common',
     'posts',
 )
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
