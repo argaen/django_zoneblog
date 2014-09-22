61a76a7702b9e0e6b624a7849836511d94892821
argaen <manu.mirandad@gmail.com>
2014-09-22 15:59:25 +0200

    Added code highlight in html with pygments and templatetag.

---
+++ wsgi/djangozone/common/templatetags/highlights.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+from django import template
+from django.template.defaultfilters import stringfilter
+
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters.html import HtmlFormatter
+
+register = template.Library()
+
+@register.filter
+@stringfilter
+def highlight_code(html):
+
+    soup = BeautifulSoup(unicode(html))
+    preblocks = soup.findAll('pre')
+
+    for pre in preblocks:
+
+        if pre.has_key('class'):
+
+            try:
+                code = ''.join([unicode(item) for item in pre.contents])
+                lexer = get_lexer_by_name(pre['class'][0])
+                formatter = HtmlFormatter()
+                code_hl = highlight(code, lexer, formatter)
+                pre.contents = [BeautifulSoup(code_hl)]
+                pre.name = 'code'
+
+            except:
+                raise
+
+    return unicode(soup)
