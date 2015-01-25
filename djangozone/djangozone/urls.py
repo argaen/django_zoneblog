0e9b75bd2c54fa16c9c03fccf1c1a0155ae71e94
blck <manu.mirandad@gmail.com>
2015-01-25 13:55:48 +0100

    Minor fix for meta keywords.

--- djangozone/djangozone/urls.py
+++ djangozone/djangozone/urls.py
@@ -2,14 +2,12 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = patterns(
     '',
     url(r'^$', 'djzone_blog.views.contents_list', name='contents_list'),
-    url(r'^djzone_blog/', include('djzone_blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^djzone_blog/', include('djzone_blog.urls')),
 
     url(r'', include('djzone_blog.urls')),
     # url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
