fad9fb62304ec3a40b05085dc47f8cdb68ba35dc
argaen <manu.mirandad@gmail.com>
2015-04-07 11:18:02 +0200

    Author field initial value is dynamically set according to request.user (if user is not superuser).

--- zone_blog/admin.py
+++ zone_blog/admin.py
@@ -27,6 +27,30 @@ class ContentAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'author', 'published_on', 'is_published')
 
+    def get_queryset(self, request):
+        qs = super(ContentAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        else:
+            return qs.filter(author=request.user)
+
+    def formfield_for_foreignkey(self, db_field, request=None, **kwargs):
+
+        field = super(ContentAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+        if db_field.name == 'author':
+            if request.user.is_superuser:
+                field.queryset = field.queryset
+            else:
+                field.queryset = field.queryset.filter(pk=request.user.pk)
+        return field
+
+    def get_form(self, request, obj=None, **kwargs):
+        if obj is None and not request.user.is_superuser:
+            form = super(ContentAdmin, self).get_form(request, obj, **kwargs)
+            form.base_fields['author'].initial = request.user
+            return form
+        return super(ContentAdmin, self).get_form(request, obj, **kwargs)
+
 
 admin.site.register(models.Post, ContentAdmin)
 admin.site.register(models.NewsItem, ContentAdmin)
