b579498008bd0cbefa873fe9b05f58fcc6ff4b30
blck <manu.mirandad@gmail.com>
2015-06-10 23:24:50 +0200

    Improved feeds.

--- zone_blog/feeds.py
+++ zone_blog/feeds.py
@@ -3,14 +3,16 @@ from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse
 
 from taggit.models import Tag
+import datetime
 
 from models import Post
 
 
-class LatestPosts(Feed):
+class LatestPostsFeed(Feed):
     title = "Django Zone latests posts"
     link = "/"
     description_template = 'content_description.html'
+    item_enclosure_mime_type = "image/jpeg"
 
     def items(self):
         return Post.objects.filter(is_published=True)
@@ -18,13 +20,20 @@ class LatestPosts(Feed):
     def item_title(self, item):
         return item.title
 
+    def item_pubdate(self, item):
+        return datetime.datetime.combine(item.published_on, datetime.time())
+
+    def item_enclosure_url(self, item):
+        return "http://django.zone/" + item.photo.url if item.photo else ''
+
     # If models don't have get_absolute_url method, override item_link
     # def item_link(self, item):
         # return reverse('post-item', args=[item.pk])
 
 
-class Category(Feed):
+class TagFeed(Feed):
     description_template = 'content_description.html'
+    item_enclosure_mime_type = "image/jpeg"
 
     def get_object(self, request, tag):
         return get_object_or_404(Tag, name=tag)
@@ -38,5 +47,11 @@ class Category(Feed):
     def item_title(self, item):
         return item.title
 
+    def item_pubdate(self, item):
+        return datetime.datetime.combine(item.published_on, datetime.time())
+
     def items(self, obj):
         return Post.objects.filter(is_published=True, tags__name=obj)
+
+    def item_enclosure_url(self, item):
+        return "http://django.zone/" + item.photo.url if item.photo else ''
--- zone_blog/urls.py
+++ zone_blog/urls.py
@@ -5,14 +5,14 @@ import feeds
 
 urlpatterns = patterns(
     '',
-    url(r'^posts$', views.PostListView.as_view(), name='post-list'),
-    url(r'^posts/rss/', feeds.LatestPosts()),
-    url(r'^posts/rss/(?P<tag>[\ \w-]+)/$', feeds.Category()),
+    url(r'^posts/$', views.PostListView.as_view(), name='post-list'),
+    url(r'^posts/rss/$', feeds.LatestPostsFeed()),
+    url(r'^posts/rss/(?P<tag>[\ \w-]+)/$', feeds.TagFeed()),
     url(r'^posts/tag/(?P<tag>[\ \w-]+)/$', views.PostListView.as_view(), name='post-list-tag'),
     url(r'^posts/archive/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.PostListView.as_view(), name='post-list-archive'),
     url(r'^posts/(?P<slug>[\w-]+)/$', views.PostDetailView.as_view(), name='post-detail'),
 
-    url(r'^projects$', views.ProjectListView.as_view(), name='project-list'),
+    url(r'^projects/$', views.ProjectListView.as_view(), name='project-list'),
     url(r'^projects/tag/(?P<tag>[\ \w-]+)/$', views.ProjectListView.as_view(), name='project-list-tag'),
     url(r'^projects/archive/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/$', views.ProjectListView.as_view(), name='project-list-archive'),
     url(r'^projects/(?P<slug>[\w-]+)/$', views.ProjectDetailView.as_view(), name='project-detail'),
